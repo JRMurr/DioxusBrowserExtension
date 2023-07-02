680db9e92c58ea3ede9420bbd681d938f37d3c21
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-07-02 15:32:24 -0400

    styling mostly good

--- crates/app/src/components/selected_stream.rs
+++ crates/app/src/components/selected_stream.rs
@@ -6,25 +6,26 @@ pub struct SelectedStreamProps<'a> {
 }
 
 pub fn selected_stream<'a>(cx: Scope<'a, SelectedStreamProps<'a>>) -> Element {
+    // TODO: make test left side, x on right side
     cx.render(rsx!(div {
         button {
-            class: "text-white bg-zinc-800 border-0 py-2 px-6 focus:outline-none  rounded text-lg",
-            "eeee: {cx.props.account}"
-            cross_icon {}
+            class: "inline-flex items-center text-white bg-zinc-800 border-0 py-2 px-6 focus:outline-none rounded text-lg",
+            "{cx.props.account}" cross_icon {}
         }
     }))
 }
 
 fn cross_icon(cx: Scope) -> Element {
     cx.render(rsx!(
-        svg { class: "flex-no-shrink fill-current",
+        svg {
             fill: "none",
             stroke: "currentColor",
             stroke_linecap: "round",
             stroke_linejoin: "round",
             stroke_width: "2",
-            // // class: " text-white p-2 bg-indigo-500 rounded-full",
             view_box: "0 0 20 20",
+            width: "5%",
+            height: "auto",
             path { stroke_linecap:"round", stroke_linejoin:"round", stroke_width:"2", d: "M6 18L18 6M6 6l12 12"}
         }
     ))
